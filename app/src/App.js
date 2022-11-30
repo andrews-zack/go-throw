d009d7dc74d7b064e97c02e8c74f95f98ca9f0d3
andrews-zack <zachary.andrews.au19@gmail.com>
2022-11-30 16:42:35 +0000

    pulling api data

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import Homepage from './components/Homepage'
-import LogIn from './components/LogIn'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Homepage from './components/Homepage';
+import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 import CourseSelect from './components/CourseSelect';
 
 
 function App() {
 
-    // const [ data, setData ] = useState([]);
+    const [ data, setData ] = useState([]);
     const [ page, setPage ] = useState('Homepage');
+
+    const url = 'https://8000-andrewszack-gothrowdb-rxyuwddajv2.ws-us77.gitpod.io/api/courses/';
+
+    useEffect(() => {
+        async function getData() {
+            const resp = await axios.get(url);
+            setData(resp.data);
+        }
+        getData();
+    }, []);
 
 
     return(
@@ -16,7 +27,7 @@ function App() {
             {page === 'Homepage' && <Homepage handleClick={setPage}/>}
             {page === 'LogIn' && <LogIn/>}
             {page === 'SignUp' && <SignUp/>}
-            {page === 'CourseSelect' && <CourseSelect/>}
+            {page === 'CourseSelect' && <CourseSelect courses={data}/>}
         </>
     )
 };
--- app/src/components/CourseSelect.js
+++ app/src/components/CourseSelect.js
@@ -1,13 +1,25 @@
 
 
 
-function CourseSelect() {
-
+function CourseSelect(props) {
+    let courseList = props.courses
+    let courseItems = courseList.map((item) =>
+        <div className="col-sm-6 col-lg-6 py-1">
+            <div className="card border border-dark h-100">
+                <div className="card-body">
+                    <h5 className="card-title fs-2 fw-bold">{item.course_name}</h5>
+                    <p className="card-text fs-4">{item.holes}</p>
+                </div>
+            </div>
+        </div>
+    )
 
     return(
-        <>
-            <p>Course Select page will go here</p>
-        </>
+        <div className="container mt-3 pt-3" id="cont">
+            <div className="row">
+                {courseItems}
+            </div>
+        </div>
     )
 }
 
