7c3204a0de873299c5ba2f92550a9fb993295156
andrews-zack <zachary.andrews.au19@gmail.com>
2022-12-06 23:27:36 +0000

    pushing to github

--- app/src/components/MapSnip.js
+++ app/src/components/MapSnip.js
@@ -5,6 +5,7 @@ import { GoogleMap, useLoadScript } from '@react-google-maps/api'
 let center = {}
 
 export default function MapSnip({map, count}) {
+    console.log(map[0].hole_list[count])
     const key = process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY
     center = {lat: map[0].hole_list[count].hole_lat, lng: map[0].hole_list[count].hole_long}
     const { isLoaded } = useLoadScript({
